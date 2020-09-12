2ce2c80970b7e4bce51e102dc4c39189be3e6211
conet.bgu@gmail.com <nAti2006>
2020-09-13 01:00:59 +0300

    first file...

---
+++ ex1/ex1.py
@@ -0,0 +1,30 @@
+
+"""
+ex1 - המרת גיל לחודשים
+"""
+x = float(input("Hi, Enter your age please: "))
+age = 12*x
+print(f"Your age in months is: {age} months")
+
+
+"""
+ex2 - 
+"""
+x = float(input("Hi, Enter your age in months please: "))
+age = x/12
+print(f"Your age in years is: {age} years")
+
+"""
+ex3
+"""
+
+x = float(input("Enter a number pleas: "))
+if x % 7 == 0:
+    print("Boom")
+else:
+    print("The number is not divisible by 7 ")
+
+"""
+ex4
+"""
+
