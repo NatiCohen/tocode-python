88aa5993c53f12d72914485207edf65a0abef25c
conet.bgu@gmail.com <nAti2006>
2020-09-14 11:46:05 +0300

    prepared file

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,4 +1,4 @@
-
+'''
 """
 ex1 - המרת גיל לחודשים
 """
@@ -27,4 +27,29 @@ else:
 """
 ex4
 """
+x = int(input("enter a number pleas: "))
+if x % 7 == 0:
+    print("Boom")
+elif '7' in str(x):
+    print("Boom")
+else:
+    print("a not 7's number")
+
+"""ex5"""
+x = int(input("Enter 3 Number pleas: \n1: "))
+y = int(input("2: "))
+z = int(input("3: "))
+print(max(x, y, z))
+'''
+
+"""
+ex6
+"""
+print("We want to calculate the sum of an invoice series")
+a1 = int(input("enter a first number of the series:\na1:= "))
+d = int(input("enter a The series difference of the series:\nd:= "))
+n = int(input("Write down the number of members of the series:\nn:= "))
+
+sumOfSeries = (2*a1 + (n-1)*d)*(n/2)
+print(f"the sum of series is {sumOfSeries}")
 
